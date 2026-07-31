d142dc269a17de7a66da0042a3afe49ecd676ff1
rare <85882771+xqxzcvlpqodkf@users.noreply.github.com>
2026-07-30 20:45:52 -0400

    Update ZombieSurvivalGarrysMod.lua

--- games/ZombieSurvivalGarrysMod.lua
+++ games/ZombieSurvivalGarrysMod.lua
@@ -1,1 +1,19 @@
+--!nocheck
 
+if not getgenv().AUTH_TABLE then
+  getgenv().LOCAL_NOTIFICATION("Error", "F9 for more details", 9999)
+  return warn("[Script Error]: error in loader initialization)
+end
+
+if not gethwid then
+  getgenv().LOCAL_NOTIFICATION("Error", "F9 for more details", 9999)
+  return warn("[Script Error]: cannot validate authentication")
+end
+
+if not table.find(getgenv().AUTH_TABLE, gethwid()) then
+  getgenv().LOCAL_NOTIFICATION("Error", "F9 for more details", 9999)
+  return warn("[Script Error]: not authenticated")
+end
+
+local obfuscated_src;
+if obfuscated_src then obfuscated_src() end
